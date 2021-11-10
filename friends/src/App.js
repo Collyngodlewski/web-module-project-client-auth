d26b82fffcb4ebf22650fab18599d91de7bfa2ec
Collyngodlewski <collyngodlewski@gmail.com>
2021-11-10 18:43:19 -0500

    i had to watch video

--- friends/src/App.js
+++ friends/src/App.js
@@ -6,12 +6,34 @@ import { BrowserRouter as Router, Route} from 'react-router-dom';
 const Login = ()=> {
   return (<h2>Login</h2>)
 }
-
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Login from './components/login';
+import PrivateRoute from './components/PrivateRoute';
+import FriendsList from './components/FriendsList';
+import AddFriendForm from './components/AddFriendForm';
 function App() {
   return (
+    <Router>
     <div className="App">
       <h2>Client Auth Project</h2>
+      <Link to='/friends'>See Friends </Link>
+      <Link to='/friends/add-new'>Add Friend</Link>
+     
+      <Switch>
+      <PrivateRoute path='/friends/add-new' component={AddFriendForm}/>
+      <PrivateRoute path='/friends' component={FriendsList}/>
+    
+
+      <Route path="/login">
+        <Login />
+      </Route>
+
+      <Route>
+        <Login />
+      </Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
